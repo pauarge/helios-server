a0cd9b8bef1d45eb040e9db9f0726d5f9b1f5a3a
Pau Argelaguet <pau@outlook.com>
2021-11-23 15:45:20 +0100

    Removing new line

--- helios/views.py
+++ helios/views.py
@@ -727,7 +727,6 @@ def one_election_cast_confirm(request, election):
         'bad_voter_login': bad_voter_login})
       
   if request.method == "POST":
-
     # voting has not started or has ended
     if (not election.voting_has_started()) or election.voting_has_stopped():
       return HttpResponseRedirect(settings.URL_HOST)
